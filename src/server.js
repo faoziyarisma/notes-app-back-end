fc937b079e2c3bddbaffb8d93474566f50f426cc
Risma Faoziya <rismafaoziya@gmail.com>
2024-05-13 15:44:06 +0700

    fix host in server.js and setting runnning application dev and prod

--- src/server.js
+++ src/server.js
@@ -1,24 +1,26 @@
-console.log('Bismillahirrahmanirrahiiim sukses & lancar serta diridloi Allah aaamiiinnnn ^-^');
-console.log('Halo kita akan membuat RESTful API');
+console.log(
+  "Bismillahirrahmanirrahiiim sukses & lancar serta diridloi Allah aaamiiinnnn ^-^"
+);
+console.log("Halo kita akan membuat RESTful API");
 
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes'); // Adjust the path if necessary
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes"); // Adjust the path if necessary
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ['*'], // Allow requests from any origin
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: 3000,
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"], // Allow requests from any origin
+      },
+    },
+  });
 
-    server.route(routes);
+  server.route(routes);
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
